2f60e544e1af8fd86386a7cb042979c2da08dc32
Anupama Ravindran <anupamaravindran@Anupamas-MacBook-Air.local>
2022-06-03 10:24:26 +0530

    react form register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DataService } from '../services/data.service';
 
@@ -15,9 +15,9 @@ export class RegisterComponent implements OnInit {
 
  //form group
  registerForm = this.fb.group({
-   acno :'',
-   pswd : '',
-   uname : ''
+   acno :[''],
+   pswd : [''],
+   uname :['',[Validators.required,Validators.pattern('[a-zA-Z ]*')]]
  })
 
   constructor(private ds: DataService, private router: Router, private fb:FormBuilder) { }
@@ -25,18 +25,26 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {
   }
   register() {
-    var uname = this.uname;
-    var acno = this.acno;
-    var pswd = this.pswd;
-    const result = this.ds.register(uname, acno, pswd)
+    var uname = this.registerForm.value.uname;
+    var acno = this.registerForm.value.acno;
+    var pswd = this.registerForm.value.pswd;
     
-    if (result) {
-      alert("Successfully Login")
-      this.router.navigateByUrl("")
-    } 
-    else {
-      alert("Already existing user...Please Log In!!")
+    if(this.registerForm.valid){
+
+      const result = this.ds.register(uname, acno, pswd)
+    
+      if (result) {
+        alert("Successfully Login")
+        this.router.navigateByUrl("")
+      } 
+      else {
+        alert("Already existing user...Please Log In!!")
+      }
+
+    }else{
+      alert("Please fill all the field")
     }
+   
   }
 
 }
